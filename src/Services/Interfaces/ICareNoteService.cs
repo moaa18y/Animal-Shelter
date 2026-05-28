2358badf01617f119d2749d210d18f24d4ff412b
Ahmed Anani <111708885+AhmedAnani@users.noreply.github.com>
2026-05-28 15:20:22 +0300

    Delete src/Services/Interfaces/ICareNoteService.cs

--- src/Services/Interfaces/ICareNoteService.cs
+++
@@ -1,8 +0,0 @@
-namespace AnimalShelter.src.Services.Interfaces
-{
-    public interface ICareNoteService
-    {
-        void AddCareNote(int animalId, string note);
-        void DeleteCareNote(int careNoteId);
-    }
-}
